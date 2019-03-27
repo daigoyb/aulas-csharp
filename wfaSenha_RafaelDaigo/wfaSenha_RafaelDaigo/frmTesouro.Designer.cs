4160ccdfd3a36a179e1430ac57fdf91bf307d187
Rafael Daigo <rafaeldaigo.yb@gmail.com>
2019-03-27 19:16:21 -0300

    projeto senha terminado

---
+++ wfaSenha_RafaelDaigo/wfaSenha_RafaelDaigo/frmTesouro.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace wfaSenha_RafaelDaigo
+{
+    partial class frmTesouro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTesouro));
+            this.SuspendLayout();
+            // 
+            // frmTesouro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(559, 330);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.Name = "frmTesouro";
+            this.Text = "T E S O U R O";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
